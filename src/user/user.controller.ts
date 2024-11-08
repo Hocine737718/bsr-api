12617bb6e8bbac394c288ea106e9a2a9f4e68605
Hocine737718 <hocine737718@gmail.com>
2024-11-08 11:04:54 +0100

    disable user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,28 +9,28 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
-  @Post()
-  create(@Body() data: CreateUserDto) {
-    return this.userService.create(data);
-  }
+  // @Post()
+  // create(@Body() data: CreateUserDto) {
+  //   return this.userService.create(data);
+  // }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.userService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.userService.findOne(id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() data: UpdateUserDto) {
-    return this.userService.update(id, data);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() data: UpdateUserDto) {
+  //   return this.userService.update(id, data);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.userService.remove(id);
+  // }
 }
